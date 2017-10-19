e370097f3595ee1ba1541ff7f72ad3498173baad
dllo <dllo@limingyu.local>
2017-10-19 19:29:24 +0800

    添加了myForEach原型方法，并实现对数组的操作

--- forEach.js
+++ forEach.js
@@ -1,9 +1,26 @@
 var array = [10,20,30,40,50];
 
 //原型本身
-array.forEach(
-    function (value,index,arr) {
-        console.log(value,index,arr);
+// array.forEach(
+//     function (value,index,arr) {
+//         console.log(value,index,arr);
+//     }
+// )
+
+// 添加原型
+Array.prototype.myForEach = function (callback,thisArg) {
+    //判断类型是否为函数
+    if (typeof callback !== "function"){
+        throw new TypeError(callback + "is not a function");
     }
-)
+    if (Object.prototype.hasOwnProperty.call(this,i)){
+        callback(this[i],i,this);
+    }
+}
+
+array.myForEach(function (value,index,arr) {
+    console.log(value);
+    console.log(index);
+    console.log(arr);
+})
 
